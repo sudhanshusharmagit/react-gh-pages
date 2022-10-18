c2db35f4311fba78e110ef52608e27cc3178543f
Sudhanshu Sharma <sudhanshu.harsh@gmail.com>
2022-10-17 20:08:45 -0400

    Updates

---
+++ precache-manifest.b120fe29aceed402cdd7175bb559fc4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0dc2adf46e6f1c834eef",
+    "url": "/react-gh-pages/static/css/main.058ddc45.chunk.css"
+  },
+  {
+    "revision": "0dc2adf46e6f1c834eef",
+    "url": "/react-gh-pages/static/js/main.0dc2adf4.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/react-gh-pages/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "fe9e49dcc9c458a203f8",
+    "url": "/react-gh-pages/static/js/2.fe9e49dc.chunk.js"
+  },
+  {
+    "revision": "f40fcb995391515f5ca8",
+    "url": "/react-gh-pages/static/js/runtime~main.f40fcb99.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-gh-pages/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "c2778c380c3aea7b2a06623fb0224c89",
+    "url": "/react-gh-pages/index.html"
+  }
+];
